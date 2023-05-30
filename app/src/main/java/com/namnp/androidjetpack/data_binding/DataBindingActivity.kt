9efe3a0ba01073089dea3dcabad290edeeaebd7f
namnpse <nam444479@gmail.com>
2023-05-30 21:05:53 +0700

    after using data binding

--- app/src/main/java/com/namnp/androidjetpack/data_binding/DataBindingActivity.kt
+++ app/src/main/java/com/namnp/androidjetpack/data_binding/DataBindingActivity.kt
@@ -2,13 +2,30 @@ package com.namnp.androidjetpack.data_binding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.namnp.androidjetpack.R
-//import com.namnp.androidjetpack.databinding.ActivityDataBindingBinding
+import com.namnp.androidjetpack.databinding.ActivityDataBindingBinding
+
+// Steps:
+// 1. Enable data binding in gradle file
+// 2. Wrap xml with <layout> tag
+// 3. Create data binding object using DataBindingUtil
+// 4. Eliminate findViewById()
 
 class DataBindingActivity : AppCompatActivity() {
-//    private lateinit var binding: ActivityDataBindingBinding
+    private lateinit var binding: ActivityDataBindingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_data_binding)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_data_binding)
+
+        binding.submitButton.setOnClickListener {
+            displayGreeting()
+        }
+    }
+
+    private fun displayGreeting() {
+        binding.apply {
+            greetingTextView.text = "Hello! " + nameEditText.text
+        }
     }
 }
