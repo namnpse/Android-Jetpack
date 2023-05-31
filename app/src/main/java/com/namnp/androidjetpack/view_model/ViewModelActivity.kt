821848371baa1f3a80240c4681c4873b611b09fa
namnpse <nam444479@gmail.com>
2023-05-31 09:03:48 +0700

    way 2 to init view model instance, using extension

--- app/src/main/java/com/namnp/androidjetpack/view_model/ViewModelActivity.kt
+++ app/src/main/java/com/namnp/androidjetpack/view_model/ViewModelActivity.kt
@@ -2,6 +2,7 @@ package com.namnp.androidjetpack.view_model
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.namnp.androidjetpack.R
@@ -9,12 +10,26 @@ import com.namnp.androidjetpack.databinding.ActivityViewModelBinding
 
 class ViewModelActivity : AppCompatActivity() {
     private lateinit var binding: ActivityViewModelBinding
+    // 2 ways of init a view model
+    // C1
     private lateinit var viewModel: CountViewModel
+    // C2: using extension
+//    For Activities.
+//    implementation 'androidx.activity:activity-ktx:1.3.1'
+//    For Fragments
+//    implementation 'androidx.fragment:fragment-ktx:1.3.1'
+
+    // Get a reference to the ViewModel scoped to this Fragment
+    //    val viewModel by viewModels<MyViewModel>()
+    // Get a reference to the ViewModel scoped to its Activity
+    //    val viewModel by activityViewModels<MyViewModel>()
+    private val viewModelUsingExtension: CountViewModel by viewModels()
     private lateinit var viewModelFactory: CustomViewModelFactory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_view_model)
         viewModelFactory = CustomViewModelFactory(startingTotal = 10)
+        // C1
         viewModel = ViewModelProvider(this, viewModelFactory)[CountViewModel::class.java]
 
         // Ex 1
