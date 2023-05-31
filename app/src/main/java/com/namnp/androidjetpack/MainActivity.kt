9a767a08f52abdf89b4a9096070c3f0cff24b189
namnpse <nam444479@gmail.com>
2023-05-31 18:52:38 +0700

    setup navigation

--- app/src/main/java/com/namnp/androidjetpack/MainActivity.kt
+++ app/src/main/java/com/namnp/androidjetpack/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.namnp.androidjetpack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.namnp.androidjetpack.data_binding.TwoWayBindingActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent = Intent(this, TwoWayBindingActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/namnp/androidjetpack/navigation/NavigationActivity.kt
@@ -0,0 +1,12 @@
+package com.namnp.androidjetpack.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.namnp.androidjetpack.R
+
+class NavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+    }
+}
