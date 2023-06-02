ccd89faa03edee75d688c4a8bb178eb952a05dff
namnpse <nam444479@gmail.com>
2023-06-02 20:05:33 +0700

    Room migration with specifications

--- app/src/main/java/com/namnp/androidjetpack/room/Subscriber.kt
+++ app/src/main/java/com/namnp/androidjetpack/room/Subscriber.kt
@@ -14,7 +14,7 @@ data class Subscriber(
     var name: String,
     @ColumnInfo("subscriber_email")
     var email: String,
-    @ColumnInfo("subscriber_phone", defaultValue = "No phone") // phone is not null -> use default value for auto migration
+    @ColumnInfo("subscriber_phone_number", defaultValue = "No phone") // phone is not null -> use default value for auto migration
     var phone: String,
     @ColumnInfo("subscriber_address") // address is nullable -> don't need to use default value for auto migration
     var address: String? = null,
--- app/src/main/java/com/namnp/androidjetpack/room/SubscriberDatabase.kt
+++ app/src/main/java/com/namnp/androidjetpack/room/SubscriberDatabase.kt
@@ -3,17 +3,29 @@ package com.namnp.androidjetpack.room
 import android.content.Context
 import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.RenameColumn
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.AutoMigrationSpec
 
 @Database(
     entities = [Subscriber::class],
-    version = 3,
-    autoMigrations = [AutoMigration(from = 2, to = 3)]
+    version = 6,
+    // listing all the migration here
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2),
+        AutoMigration(from = 2, to = 3),
+        AutoMigration(from = 3, to = 4),
+        AutoMigration(from = 4, to = 5),
+        AutoMigration(from = 5, to = 6, spec = SubscriberDatabase.Migration5To6::class),
+    ]
 )
 abstract class SubscriberDatabase : RoomDatabase() {
 
     abstract val subscriberDAO: SubscriberDAO
+
+    @RenameColumn(tableName = "subscriber_table", fromColumnName = "phone", toColumnName = "phone_number")
+    class Migration5To6 : AutoMigrationSpec
 
     companion object {
         @Volatile
