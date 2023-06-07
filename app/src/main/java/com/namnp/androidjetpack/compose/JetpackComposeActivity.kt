ccad01ad327f4c66afa343f6cd645dc56536ee85
namnpse <nam444479@gmail.com>
2023-06-07 08:49:46 +0700

    display tv show via lazy column, sub-compose

--- app/src/main/java/com/namnp/androidjetpack/compose/JetpackComposeActivity.kt
+++ app/src/main/java/com/namnp/androidjetpack/compose/JetpackComposeActivity.kt
@@ -1,6 +1,7 @@
 package com.namnp.androidjetpack.compose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
@@ -13,7 +14,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,11 +33,16 @@ class JetpackComposeActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colors.background
                 ) {
 //                    ScrollableColumn()
-                    LazyColumnCompose() {
-
+//                    LazyColumnCompose() {
+//
+//                    }
+                    DisplayTvShows {
+                        Toast.makeText(this, it.name, Toast.LENGTH_LONG).show()
+                        startActivity(InfoActivity.intent(this, it))
+//                }
                     }
                 }
             }
@@ -54,7 +60,7 @@ fun ScrollableColumn() {
         for (i in 1..20) {
             Text(
                 "User Name $i",
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.h3,
                 modifier = Modifier.padding(12.dp)
             )
             Divider(color = Color.Black, thickness = 6.dp)
@@ -63,17 +69,33 @@ fun ScrollableColumn() {
 }
 
 @Composable
-fun LazyColumnCompose(selectedItem: (String) -> (Unit)) {
+fun LazyColumnCompose(selectedItem: (String) -> Unit) {
     LazyColumn {
         items(20) {
             Text(
                 "User Name $it",
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.h3,
                 modifier = Modifier
                     .padding(12.dp)
                     .clickable { selectedItem("$it Selected") }
             )
             Divider(color = Color.Black, thickness = 6.dp)
         }
+    }
+}
+
+@Composable
+fun DisplayTvShows(selectedItem: (TvShow) -> Unit) {
+
+    val tvShows = remember { TvShowList.tvShows }
+    LazyColumn(
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        items(
+            items = tvShows,
+            itemContent = {
+                TvShowListItem(tvShow = it, selectedItem)
+            }
+        )
     }
 }
