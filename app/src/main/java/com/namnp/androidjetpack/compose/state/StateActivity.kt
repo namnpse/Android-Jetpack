ee69fc25d4c61a61be4f49be6588316f66517ead
namnpse <nam444479@gmail.com>
2023-06-08 19:48:15 +0700

    use field delegate in compose for concise syntax

--- app/src/main/java/com/namnp/androidjetpack/compose/state/StateActivity.kt
+++ app/src/main/java/com/namnp/androidjetpack/compose/state/StateActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -36,14 +38,14 @@ fun MyButton() {
 //    remember = store the value just in case recompose is called.
 //    so now when reCompose should actually be triggered? And there the mutable states come to help.
 //    2. mutableState = store the value and in case I update value trigger, recompose for all elements using this data.
-    var count = remember {
+    var count by remember {
         mutableStateOf(0)
     }
     val context = LocalContext.current
     Button(
         onClick = {
-            count.value = count.value+1
-            Toast.makeText(context, "${count.value}", Toast.LENGTH_LONG).show()
+            count = count+1
+            Toast.makeText(context, "${count}", Toast.LENGTH_LONG).show()
         },
         contentPadding = PaddingValues(16.dp),
         border = BorderStroke(8.dp, Color.Black),
@@ -52,6 +54,6 @@ fun MyButton() {
             contentColor = Color.White,
         )
     ) {
-        Text(text = "Count is: ${count.value}")
+        Text(text = "Count is: ${count}")
     }
 }
