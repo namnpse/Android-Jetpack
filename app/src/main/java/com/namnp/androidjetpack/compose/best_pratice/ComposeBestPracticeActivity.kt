3f8dc020d011d29a573654e503b44ed3ea340917
namnpse <nam444479@gmail.com>
2023-06-18 19:31:23 +0700

    [Compose] Best practice: Handle navigation in Compose

--- app/src/main/java/com/namnp/androidjetpack/compose/best_pratice/ComposeBestPracticeActivity.kt
+++ app/src/main/java/com/namnp/androidjetpack/compose/best_pratice/ComposeBestPracticeActivity.kt
@@ -17,10 +17,10 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.namnp.androidjetpack.compose.best_pratice.navigation.AppNavHost
 import com.namnp.androidjetpack.compose.ui.theme.AndroidJetpackTheme
 import kotlinx.coroutines.flow.collectLatest
 
-// Handling events with sealed classes
 class ComposeBestPracticeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,97 +30,100 @@ class ComposeBestPracticeActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                  MainScreen()
+//                  MainScreen()
+                    AppNavHost()
                 }
             }
         }
     }
 }
 
+
+// Handling events with sealed classes
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen(
     modifier: Modifier = Modifier,
     counterViewModel: CounterViewModel = viewModel()
-){
+) {
 
-  val screenState = counterViewModel.screenState.value
-  val scaffoldState = rememberScaffoldState()
+    val screenState = counterViewModel.screenState.value
+    val scaffoldState = rememberScaffoldState()
 
-   LaunchedEffect(key1 = true ){
-        counterViewModel.uiEventFlow.collectLatest {event->
-             when(event){
-                 is UIEvent.ShowMessage -> {
-                   scaffoldState.snackbarHostState.showSnackbar(
-                       message = event.message
-                   )
-                 }
-             }
+    LaunchedEffect(key1 = true) {
+        counterViewModel.uiEventFlow.collectLatest { event ->
+            when (event) {
+                is UIEvent.ShowMessage -> {
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        message = event.message
+                    )
+                }
+            }
         }
-   }
+    }
 
 
 
-  Scaffold(scaffoldState = scaffoldState) {
-      Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(50.dp),
-        verticalArrangement = Arrangement.SpaceEvenly
-      ) {
-         Text(
-             modifier = modifier.fillMaxWidth(),
-             text = screenState.displayingResult,
-             fontWeight = FontWeight.Bold,
-             fontSize = 30.sp,
-             color = Color.DarkGray
-         )
-          OutlinedTextField(
-              value = screenState.inputValue,
-              onValueChange = {
-                  counterViewModel.onEvent(CounterEvent.ValueEntered(it))
-              },
-              modifier = modifier.fillMaxWidth(),
-              keyboardOptions = KeyboardOptions(
-                  keyboardType = KeyboardType.Number
-              ),
-              textStyle = TextStyle(
-                  color = Color.LightGray,
-                  fontSize = 30.sp,
-                  fontWeight = FontWeight.Bold
-              ),
-              label = { Text(text = "New Count")}
-          )
-          if(screenState.isCountButtonVisible){
-              Button(
-                  onClick = {
-                      counterViewModel.onEvent(CounterEvent.CountButtonClicked)
-                  },
-                  modifier = modifier.fillMaxWidth()
-              )
-              {
-               Text(
-                   text = "Count",
-                   fontSize = 30.sp,
-                   fontWeight = FontWeight.Bold
-               )
-              }
-          }
-          Button(
-              onClick = {
-                  counterViewModel.onEvent(CounterEvent.ResetButtonClicked)
-              },
-              modifier = modifier.fillMaxWidth()
-          ) {
-              Text(
-                  text = "Reset",
-                  fontSize = 30.sp,
-                  fontWeight = FontWeight.Bold
-              )
-          }
+    Scaffold(scaffoldState = scaffoldState) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(50.dp),
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                modifier = modifier.fillMaxWidth(),
+                text = screenState.displayingResult,
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp,
+                color = Color.DarkGray
+            )
+            OutlinedTextField(
+                value = screenState.inputValue,
+                onValueChange = {
+                    counterViewModel.onEvent(CounterEvent.ValueEntered(it))
+                },
+                modifier = modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                ),
+                textStyle = TextStyle(
+                    color = Color.LightGray,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+                label = { Text(text = "New Count") }
+            )
+            if (screenState.isCountButtonVisible) {
+                Button(
+                    onClick = {
+                        counterViewModel.onEvent(CounterEvent.CountButtonClicked)
+                    },
+                    modifier = modifier.fillMaxWidth()
+                )
+                {
+                    Text(
+                        text = "Count",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+            Button(
+                onClick = {
+                    counterViewModel.onEvent(CounterEvent.ResetButtonClicked)
+                },
+                modifier = modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Reset",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
 
-      }
-  }
+        }
+    }
 
 }
 
