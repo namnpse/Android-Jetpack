02610f3e85cc54c0313694cf5d6f72a13803ceb3
namnpse <nam444479@gmail.com>
2023-06-19 14:18:35 +0700

    [Testing] test calculation implementation

--- app/src/main/java/com/namnp/androidjetpack/testing/CalculationActivity.kt
+++ app/src/main/java/com/namnp/androidjetpack/testing/CalculationActivity.kt
@@ -22,3 +22,16 @@ class CalculationActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
     }
 }
+
+// NOTE
+//1. Testing pyramid: 70-20-10
+//2. Test doubles (Fake, Stub, Mock)
+//      2.1 Fake:
+//          + a light weight implementation class of the Interface,
+//          + usually we hand code fake classes. So the Subject under test can use the fake instead of the real one.
+
+//      2.2: Stub is an object that provides predefined return values to method calls.
+//      2.3:
+//          + Mock is similar to stub, but they allows tester to set answers to method calls when writing the test case.
+//          + That means In mocks we dynamically set expected return values for the method calls.
+//          + Not like fakes, we usually generate stubs and mocks using a testing framework.
