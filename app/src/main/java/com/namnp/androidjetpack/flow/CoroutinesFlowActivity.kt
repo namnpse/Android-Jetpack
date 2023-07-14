64421eacf09bd9f3818dd8c77ed11d69cc930834
namnpse <nam444479@gmail.com>
2023-07-14 17:36:18 +0700

    [Kotlin Flow] replace launchWhenStarted by repeatOnLifecycle(), add extension to safe collect flow

--- app/src/main/java/com/namnp/androidjetpack/flow/CoroutinesFlowActivity.kt
+++ app/src/main/java/com/namnp/androidjetpack/flow/CoroutinesFlowActivity.kt
@@ -1,6 +1,6 @@
 package com.namnp.androidjetpack.flow
 
-import androidx.lifecycle.viewmodel.compose.viewModel
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,10 +10,17 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.namnp.androidjetpack.compose.ui.theme.AndroidJetpackTheme
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class ComposeStateActivity : ComponentActivity() {
+    @SuppressLint("CoroutineCreationDuringComposition")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,6 +36,20 @@ class ComposeStateActivity : ComponentActivity() {
 
                 }
             }
+//            --> launchWhenStarted deprecated
+//            --> use repeatOnLifecycle(Lifecycle.State.STARTED)
+
+//            -> more optimal, safer way
+            collectLatestLifecycleFlow(viewModel.stateFlow) {
+
+            }
+            lifecycleScope.launch {
+                repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    viewModel.stateFlow.collect {
+
+                    }
+                }
+            }
             val value = viewModel.myFlow.collectAsState(initial = 0)
             AndroidJetpackTheme {
                 Surface(
@@ -42,3 +63,13 @@ class ComposeStateActivity : ComponentActivity() {
 
     }
 }
+
+
+fun <T> ComponentActivity.collectLatestLifecycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collectLatest(collect)
+        }
+    }
+}
+
