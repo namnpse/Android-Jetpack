0fa41d124c66bc7041424a6c7b07fa9423785ee4
namnpse <nam444479@gmail.com>
2023-08-09 19:22:14 +0700

    [Coroutine] threads and dispatchers, unconfined dispatcher, switch threads

---
+++ app/src/main/java/com/namnp/androidjetpack/coroutines/ContextAndDispatcher.kt
@@ -0,0 +1,76 @@
+package com.namnp.androidjetpack.coroutines
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+fun main() {
+    dispatchersAndThread()
+}
+
+fun dispatchersAndThread() = runBlocking<Unit> {
+    launch { // context of the parent, main runBlocking coroutine
+        println("main runBlocking      : I'm working in thread ${Thread.currentThread().name}")
+    }
+    launch(Dispatchers.Unconfined) { // not confined -- will work with main thread
+        println("Unconfined            : I'm working in thread ${Thread.currentThread().name}")
+    }
+    launch(Dispatchers.Default) { // will get dispatched to DefaultDispatcher
+        println("Default               : I'm working in thread ${Thread.currentThread().name}")
+    }
+    // creates a thread for the coroutine to run -> expensive, have to release when not needed
+    launch(newSingleThreadContext("MyOwnThread")) { // will get its own new thread
+        println("newSingleThreadContext: I'm working in thread ${Thread.currentThread().name}")
+    }
+}
+/*
+Unconfined            : I'm working in thread main
+Default               : I'm working in thread DefaultDispatcher-worker-1
+newSingleThreadContext: I'm working in thread MyOwnThread
+main runBlocking      : I'm working in thread main
+*/
+
+fun unconfinedDispatcher() = runBlocking<Unit> {
+    launch(Dispatchers.Unconfined) { // not confined -- will work with main thread
+        println("Unconfined      : I'm working in thread ${Thread.currentThread().name}")
+        delay(500)
+        // resumes in the default executor thread that the delay function is using
+        println("Unconfined      : After delay in thread ${Thread.currentThread().name}")
+    }
+    launch { // inherits context of the parent, main runBlocking coroutine
+        println("main runBlocking: I'm working in thread ${Thread.currentThread().name}")
+        delay(1000)
+        println("main runBlocking: After delay in thread ${Thread.currentThread().name}")
+    }
+}
+/*
+Unconfined      : I'm working in thread main
+main runBlocking: I'm working in thread main
+Unconfined      : After delay in thread kotlinx.coroutines.DefaultExecutor (resumes in the default executor thread that the delay function is using)
+main runBlocking: After delay in thread main
+*/
+
+fun log(msg: String) = println("[${Thread.currentThread().name}] $msg")
+
+fun switchingBetweenThreads() {
+    newSingleThreadContext("Ctx1").use { ctx1 ->
+        newSingleThreadContext("Ctx2").use { ctx2 ->
+            runBlocking(ctx1) {
+                log("Started in ctx1")
+                withContext(ctx2) {
+                    log("Working in ctx2")
+                }
+                log("Back to ctx1")
+            }
+        }
+    }
+}
+
+/*
+[Ctx1 @coroutine#1] Started in ctx1
+[Ctx2 @coroutine#1] Working in ctx2
+[Ctx1 @coroutine#1] Back to ctx1
+*/
