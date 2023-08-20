3a495805d543ce3cb81e417de04e859861dcd3ea
namnpse <nam444479@gmail.com>
2023-08-20 21:30:59 +0700

    [Navigation, Compose] passing multiple values, optional values (?) using navigation arguments

--- app/src/main/java/com/namnp/androidjetpack/compose/sharing_data/NavigationArguments.kt
+++ app/src/main/java/com/namnp/androidjetpack/compose/sharing_data/NavigationArguments.kt
@@ -1,5 +1,6 @@
 package com.namnp.androidjetpack.compose.sharing_data
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -10,6 +11,11 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 
 const val ARG_KEY_1 = "KEY_1"
+const val EXERCISE_ID = "EXERCISE_ID"
+const val WORKOUT_ID = "WORKOUT_ID"
+const val SET_NUMBER = "SET_NUMBER"
+const val REP_NUMBER = "REP_NUMBER"
+//createExercise/{exerciseId}/{workoutId}?setNumber={setNumber}&repNumber={repNumber}
 
 @Composable
 fun NavigationArgs() {
@@ -20,7 +26,10 @@ fun NavigationArgs() {
     ) {
         composable("screen1") {
             Screen1(onNavigateToScreen2 = {
-                navController.navigate("screen2/$it")
+//                navController.navigate("screen2/$it")
+//                navController.navigate("createExercise/Ex1/Work1")
+                navController.navigate("createExercise/Ex1/Work1?$SET_NUMBER")
+//                navController.navigate("createExercise/Ex1/Work1?$SET_NUMBER=1&$REP_NUMBER=2")
             })
         }
         composable(
@@ -29,18 +38,43 @@ fun NavigationArgs() {
                 navArgument(ARG_KEY_1) {
                     type = NavType.StringType
                 },
-                navArgument(ARG_KEY_1) {
-                    type = NavType.StringType
-                }
             )
         ) {
             val param = it.arguments?.getString(ARG_KEY_1) ?: ""
             Screen2(param = param)
         }
+        composable(
+            route = "createExercise/{$EXERCISE_ID}/{$WORKOUT_ID}?$SET_NUMBER={$SET_NUMBER}&$REP_NUMBER={$REP_NUMBER}",
+            arguments = listOf(
+                navArgument(EXERCISE_ID) {
+                    type = NavType.StringType
+                },
+                navArgument(WORKOUT_ID) {
+                    type = NavType.StringType
+                },
+                navArgument(SET_NUMBER) {
+                    type = NavType.IntType
+                    defaultValue = 123
+                },
+                navArgument(REP_NUMBER) {
+                    type = NavType.IntType
+                    defaultValue = 234
+                }
+            )
+        ) {
+            val exerciseId = it.arguments?.getString(EXERCISE_ID) ?: ""
+            val workoutId = it.arguments?.getString(WORKOUT_ID) ?: ""
+            val setNumber = it.arguments?.getInt(SET_NUMBER)
+            val repNumber = it.arguments?.getInt(REP_NUMBER)
+            CreateExercise(
+                exerciseId = exerciseId,
+                workoutId = workoutId,
+                setNumber = setNumber,
+                repNumber = repNumber,
+            )
+        }
     }
 }
-
-
 
 
 @Composable
@@ -55,4 +89,20 @@ private fun Screen1(onNavigateToScreen2: (String) -> Unit) {
 @Composable
 private fun Screen2(param: String) {
     Text(text = param)
+}
+
+@Composable
+private fun CreateExercise(
+    exerciseId: String,
+    workoutId: String,
+    setNumber: Int? = null,
+    repNumber: Int? = null,
+) {
+    Column {
+        Text(text = "CreateExercise")
+        Text(text = "exerciseId: $exerciseId")
+        Text(text = "workoutId: $workoutId")
+        Text(text = "setNumber: ${setNumber ?: "No setNumber"}")
+        Text(text = "repNumber: ${repNumber ?: "No repNumber"}")
+    }
 }
